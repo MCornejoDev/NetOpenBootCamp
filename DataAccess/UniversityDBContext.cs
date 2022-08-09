0c1cd362f093c026966e55fcca48c0f0fe0ef139
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-09 13:47:56 +0200

    Feat : "Ejercicio 1 Curso de APIs con .NET"

---
+++ DataAccess/UniversityDBContext.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.EntityFrameworkCore;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+
+        }
+
+        //TODO: Add DbSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Curso { get; set; }
+    }
+}
---
+++ Models/DataModels/Curso.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace universityApiBackend.Models.DataModels
+{
+    public enum Levels { basic, intermediate, advanced }
+
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string LongDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetPublic { get; set; } = string.Empty;
+
+        [Required]
+        public string Targets { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required, EnumDataType(typeof(Levels))]
+        public Levels Levels { get; set; }
+    }
+}
