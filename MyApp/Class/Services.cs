a417c4bc82029c5e65b85f7bf1f956ad54798da1
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-10 21:07:11 +0200

    Feat: Ejercicios LINQ finalizados

--- MyApp/Class/Services.cs
+++ MyApp/Class/Services.cs
@@ -9,39 +9,48 @@ namespace MyApp.Class
     public class Services
     {
         // Buscar usuarios por email
-        public User SearchUserByEmail(List<User> listUsers, string email) //Lista de usuarios y email
+        static public void SearchUserByEmail(List<User> listUsers, string email) //Lista de usuarios y email
         {
-            return listUsers.First(user => user.Email.Equals(email));
+            var users = listUsers.FindAll(user => user.Email.Equals(email));
         }
 
         // Buscar alumnos mayores de edad
-        static public void SearchStudentByLegalAge(string email)
+        static public void SearchStudentsByLegalAge(List<Student> listStudents)
         {
+            var students = listStudents.FindAll(user => CalculateAge(user.Dob) >= 18);
+        }
 
+        static public int CalculateAge(DateTime DoB)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - DoB.Year;
+            if (now < DoB.AddYears(age))
+                age--;
+            return age;
         }
 
         // Buscar alumnos que tengan al menos un curso
-        static public void SearchStudentWithAtLeastCourse()
+        static public void SearchStudentWithAtLeastCourse(List<Student> listStudents)
         {
-
+            var students = listStudents.FindAll(user => user.Courses.Any());
         }
 
         // Buscar cursos de un nivel determinado que al menos tengan un alumno inscrito
-        static public void SearchCoursesWithCertainLevelAndAtLeastStudent()
+        static public void SearchCoursesWithCertainLevelAndAtLeastStudent(List<Course> listCourses, string level)
         {
-
+            var courses = listCourses.FindAll(course => course.Levels.Equals(level) && course.Students.Any());
         }
 
         // Buscar cursos de un nivel determinado que sean de una categoría determinada
-        static public void SearchCoursesWithCertainLevelFromCertainCategory()
+        static public void SearchCoursesWithCertainLevelFromCertainCategory(List<Course> listCourses, string level, string category)
         {
-
+            var courses = listCourses.FindAll(course => course.Levels.Equals(level) && course.Categories.Any(category => category.Equals(category)));
         }
 
         // Buscar cursos sin alumnos
-        static public void SearchCoursesWithoutStudents()
+        static public void SearchCoursesWithoutStudents(List<Course> listCourse)
         {
-
+            var courses = listCourse.FindAll(course => course.Students.Count() == 0);
         }
 
     }
