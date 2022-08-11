4df9f96f44871a24a3f08171df6fffda379e40ba
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-11 21:43:48 +0200

    Fix: Se ha arreglado el namespace

---
+++ MyApp/Services/IStudentsService.cs
@@ -0,0 +1,11 @@
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public interface IStudentsService
+    {
+        IEnumerable<Student> GetStudentsWithCourses();
+        IEnumerable<Student> GetStudentsWithNotCourses();
+
+    }
+}
---
+++ MyApp/Services/StudentsService.cs
@@ -0,0 +1,18 @@
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public class StudentsService : IStudentsService
+    {
+        //TODO : resolve Methods
+        public IEnumerable<Student> GetStudentsWithCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsWithNotCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
