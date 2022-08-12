1ad252f8f4dc43adc911f79fc6f540cb9f456964
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-12 12:45:51 +0200

    Fix: Se ha corregido un error en el namespace y se ha añadido las interfaces, servicios y controladores faltantes

--- MyApp/Services/Services.cs
+++ MyApp/Services/Services.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using UniversityApiBackend.Models.DataModels;
 
-namespace MyApp.Class
+namespace UniversityApiBackend.Class
 {
     public class Services
     {
