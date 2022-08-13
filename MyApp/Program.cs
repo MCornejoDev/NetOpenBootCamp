a327a35cced7fcc058d4710adf682c1960648ac3
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-13 12:26:51 +0200

    Feat: Se ha añadido nueva configuración

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -12,8 +12,11 @@ var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME)
 // 3. Add Context
 builder.Services.AddDbContext<UniversityDBContext>(options => options.UseSqlServer(connectionString));
 
-// Add services to the container.
+// 7. Add Service Of JWT Autorization
+// TODO : 
+// builder.Services.AddJwtTokenService(builder.Configuration);
 
+// Add services to the container.
 builder.Services.AddControllers();
 
 // 4. Add Custom Services (folder Services)
@@ -22,6 +25,8 @@ builder.Services.AddScoped<IStudentsService, StudentsService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// 8. TODO : Config Swagger to take care of Autorization of JWT
 builder.Services.AddSwaggerGen();
 
 // 5. CORS Configuration
