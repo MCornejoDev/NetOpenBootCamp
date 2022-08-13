037265292ac5b903f3f9e56f2e42794f988b639c
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-13 12:59:08 +0200

    Feat : Se ha añadido las configuraciones necesarias para JWT de la clase 6

--- MyApp/Controllers/AccountController.cs
+++ MyApp/Controllers/AccountController.cs
@@ -1,15 +1,76 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using UniversityApiBackend.Models.DataModels;
+using UniversityApiBackend.Helpers;
 
 namespace UniversityApiBackend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/{action}")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly JwtSettings _jwtSettings;
+        public AccountController(JwtSettings jwtSettings)
+        {
+            _jwtSettings = jwtSettings;
+        }
+
+        private IEnumerable<User> Logins = new List<User>(){
+            new User()
+            {
+                Id = 1,
+                Email = "miguel@test.com",
+                Name = "Admin",
+                Password = "Admin"
+            },
+            new User(){
+                Id = 2,
+                Email = "pepe@test.com",
+                Name = "User1",
+                Password = "pepe"
+            }
+        };
+
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogin)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = Logins.Any(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+
+                if (Valid)
+                {
+                    var user = Logins.FirstOrDefault(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+
+                    Token = JwtHelpers.GenTokenKey(new UserTokens()
+                    {
+                        UserName = user.Name,
+                        EmailId = user.Email,
+                        Id = user.Id,
+                        GuidId = Guid.NewGuid()
+                    }, _jwtSettings);
+                }
+                else
+                {
+                    return BadRequest("Wrong Password");
+                }
+                return Ok(Token);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetToken Error", ex);
+            }
+        }
+
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public IActionResult GetUserList()
+        {
+            return Ok(Logins);
+        }
     }
 }
--- MyApp/Models/DataModels/UserTokens.cs
+++ MyApp/Models/DataModels/UserTokens.cs
@@ -7,7 +7,7 @@ namespace UniversityApiBackend.Models.DataModels
 {
     public class UserTokens
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Token { get; set; }
         public string UserName { get; set; }
         public TimeSpan Validity { get; set; }
