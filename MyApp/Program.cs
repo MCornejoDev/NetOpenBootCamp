a8956f16938a5e1b1e1899174a1f7819ec2468be
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-16 12:35:46 +0200

    Feat: Añadido tokens para rutas protegidas

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,6 @@
 // 1. Usings to work with EntityFramework
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using UniversityApiBackend;
 using UniversityApiBackend.DataAccess;
 using UniversityApiBackend.Services;
@@ -24,11 +25,45 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IStudentsService, StudentsService>();
 // TODO: Add the rest of services
 
+// 8. Add Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserOnlyPolicy", policy => policy.RequireClaim("UserOnly", "User1"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-// 8. TODO : Config Swagger to take care of Autorization of JWT
-builder.Services.AddSwaggerGen();
+// 9. Config Swagger to take care of Autorization of JWT
+builder.Services.AddSwaggerGen(options =>
+    {
+        //We define the Security for authorization
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization Header using Bearer Schema"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    }
+);
 
 // 5. CORS Configuration
 builder.Services.AddCors(options =>
