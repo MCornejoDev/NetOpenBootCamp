2cbd0d8811500a023107e4e2924857796cedab74
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-16 12:44:50 +0200

    Feat: Pruebas del bearer token con weatherforescast

--- MyApp/Controllers/WeatherForecastController.cs
+++ MyApp/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UniversityApiBackend.Controllers;
@@ -21,6 +23,7 @@ public class WeatherForecastController : ControllerBase
     // Method GET => Get to https://localhost:7067/weatherforecast
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator, User")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
