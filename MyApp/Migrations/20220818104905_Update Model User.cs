fb3922e8662cde4b08a4b2651ffdfcafb27a717a
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-18 12:52:11 +0200

    Feat: Se ha añadido una nueva migración

---
+++ MyApp/Migrations/20220818104905_Update Model User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityApiBackend.Migrations
+{
+    public partial class UpdateModelUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Roles",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Users");
+        }
+    }
+}
