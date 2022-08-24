a21d296959b380896642994a01771c474e3aa810
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-24 12:28:25 +0200

    Feat: Añadido configuración de logs en el dbcontext

--- MyApp/DataAccess/UniversityDBContext.cs
+++ MyApp/DataAccess/UniversityDBContext.cs
@@ -6,9 +6,11 @@ namespace UniversityApiBackend.DataAccess
 {
     public class UniversityDBContext : DbContext
     {
-        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
-        {
+        private readonly ILoggerFactory _loggerFactory;
 
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
         }
 
         //TODO: Add DbSets (Tables of our Data base)
@@ -18,5 +20,24 @@ namespace UniversityApiBackend.DataAccess
         public DbSet<Category>? Category { get; set; }
         public DbSet<Student>? Students { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+            /*These options are for doing a full log but there is problem : 
+            if you do query select of 1000 rows, you will get a log of all and worst perfomance 
+            */
+            // optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[]{
+            //     DbLoggerCategory.Database.Name
+            // }));
+            // optionsBuilder.EnableSensitiveDataLogging();
+
+            /*These options are for doing a filter out of information level where 
+            the table save the information with errors detail and sensitive data, 
+            this full option has fast perfomance
+            */
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
     }
 }
