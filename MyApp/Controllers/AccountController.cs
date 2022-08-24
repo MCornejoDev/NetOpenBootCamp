0546f9ec752e1c0db802d2fc9b68470b9685d61a
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2022-08-24 12:37:38 +0200

    Feat: Se ha añadido logging a un controlador y un servicio para tener un ejemplo de uso

--- MyApp/Controllers/AccountController.cs
+++ MyApp/Controllers/AccountController.cs
@@ -8,39 +8,44 @@ using UniversityApiBackend.Helpers;
 
 namespace UniversityApiBackend.Controllers
 {
-    [Route("api/[controller]/[action]")]
     [ApiController]
+    [Route("api/[controller]/[action]")]
     public class AccountController : ControllerBase
     {
         private readonly UniversityDBContext _context;
         private readonly JwtSettings _jwtSettings;
-        public AccountController(UniversityDBContext context, JwtSettings jwtSettings)
+        private readonly ILogger<AccountController> _logger;
+        public AccountController(UniversityDBContext context, JwtSettings jwtSettings, ILogger<AccountController> logger)
         {
             _context = context;
             _jwtSettings = jwtSettings;
+            _logger = logger;
         }
 
         //Example Users
         //TODO: Change by real users in DB
-        private IEnumerable<User> Logins = new List<User>(){
-            new User()
-            {
-                Id = 1,
-                Email = "miguel@test.com",
-                Name = "Admin",
-                Password = "Admin"
-            },
-            new User(){
-                Id = 2,
-                Email = "pepe@test.com",
-                Name = "User1",
-                Password = "pepe"
-            }
-        };
+        // private IEnumerable<User> Logins = new List<User>(){
+        //     new User()
+        //     {
+        //         Id = 1,
+        //         Email = "miguel@test.com",
+        //         Name = "Admin",
+        //         Password = "Admin"
+        //     },
+        //     new User(){
+        //         Id = 2,
+        //         Email = "pepe@test.com",
+        //         Name = "User1",
+        //         Password = "pepe"
+        //     }
+        // };
 
         [HttpPost]
         public IActionResult GetToken(UserLogins userLogin)
         {
+            _logger.LogWarning($"{nameof(AccountController)} - {nameof(GetToken)} - Warning Level Log");
+            _logger.LogError($"{nameof(AccountController)} - {nameof(GetToken)} - Error Level Log");
+            _logger.LogCritical($"{nameof(AccountController)} - {nameof(GetToken)} - Critical Level Log");
             try
             {
                 var Token = new UserTokens();
@@ -86,6 +91,9 @@ namespace UniversityApiBackend.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
         public IActionResult GetUserList()
         {
+            _logger.LogWarning($"{nameof(AccountController)} - {nameof(GetUserList)} - Warning Level Log");
+            _logger.LogError($"{nameof(AccountController)} - {nameof(GetUserList)} - Error Level Log");
+            _logger.LogCritical($"{nameof(AccountController)} - {nameof(GetUserList)} - Critical Level Log");
             // We are searching all Users
             // var searchUsers = _context.Users.ToList();
             var searchUsers = from user in _context.Users
--- MyApp/Services/StudentsService.cs
+++ MyApp/Services/StudentsService.cs
@@ -4,9 +4,20 @@ namespace UniversityApiBackend.Services
 {
     public class StudentsService : IStudentsService
     {
+        private readonly ILogger<StudentsService> _logger;
+
+        public StudentsService(ILogger<StudentsService> logger)
+        {
+            _logger = logger;
+        }
+
         //TODO : resolve Methods
         public IEnumerable<Student> GetStudentsWithCourses()
         {
+            _logger.LogWarning($"{nameof(StudentsService)} - {nameof(GetStudentsWithCourses)} - Warning Level Log");
+            _logger.LogError($"{nameof(StudentsService)} - {nameof(GetStudentsWithCourses)} - Error Level Log");
+            _logger.LogCritical($"{nameof(StudentsService)} - {nameof(GetStudentsWithCourses)} - Critical Level Log");
+
             throw new NotImplementedException();
         }
 
